7bfa8961edd3ca670776f1d533fa84f963e10755
nileshgokhalepune <nileshgokhale45@gmail.com>
2017-06-05 07:57:37 -0500

    added startup file

--- server/server.js
+++ server/server.js
@@ -13,4 +13,11 @@ app.listen(3010, function () {
     console.log('Server listening on ', 3010);
 });
 
+
+app.use(function (req, res, next) {
+    var err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
 module.exports = app;
