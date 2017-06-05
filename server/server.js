a92e691f7fe7138e44b7c0841bd4891a4a371370
ICG\ngokhal <nilesh.gokhale@ingramcontent.com>
2017-06-05 11:53:53 -0500

    fixing build

--- server/server.js
+++ server/server.js
@@ -6,13 +6,13 @@ var indexRoute = require('./routes/index');
 
 var app = express();
 app.use(bodyParser.json());
-app.use('view engine', 'jade');
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
 app.use('/', indexRoute);
-
-app.listen(3010, function () {
-    console.log('Server listening on ', 3010);
-});
-
+app.use(express.static(path.join(__dirname, 'public')));
+// app.listen(3010, function () {
+//     console.log('Server listening on ', 3010);
+// });
 
 app.use(function (req, res, next) {
     var err = new Error('Not found');
