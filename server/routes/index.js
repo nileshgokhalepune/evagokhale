cae8ff1da8376de487e50cf0de582e1876b58190
ICG\ngokhal <nilesh.gokhale@ingramcontent.com>
2017-06-27 16:06:09 -0500

    pure js style

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,33 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Family Tree' });
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Family Tree'
+  });
 });
+
+router.get('/users/:id', function(req, res, next) {
+  res.json({
+    name: 'Eva',
+    id: 1,
+    relation: 'self',
+    type: 'self',
+    imageUrl:'/assets/missin.gif',
+    family: [{
+      name: 'Priti',
+      id: 2,
+      relation: 'Mother',
+      type: 'Parent',
+    imageUrl:'/assets/missin.gif'
+    }, {
+      name: 'Nilesh',
+      id: 3,
+      relation: 'Father',
+      type: 'Parent',
+    imageUrl:'/assets/missin.gif'
+    }]
+  });
+})
 
 module.exports = router;
